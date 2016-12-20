3f6553aa6ee6040170d681b838f410628164e7b7
xmey95 <xmey95@gmail.com>
2016-12-20 12:53:25 +0100

    Make Dynamic Projects List

--- scripts.js
+++ scripts.js
@@ -6,10 +6,10 @@ global.$ = $;
 
 const {remote} = require('electron');
 const {Menu, BrowserWindow, MenuItem, shell} = remote;
-const fs = require('fs')
+const fs = require('fs-extra')
 const path = require('path')
 let addWindow
-
+showProjects();
 var App = {
   // show "add" window
   add: function () {
@@ -30,19 +30,45 @@ var App = {
   }
 };
 
-function deleteProject() {
-  dialog.showMessageBox({ message: "The project has been deleted!", buttons: ["OK"] });
+function deleteProject(filename) {
+  var path = __dirname + "/Projects/" + filename + ".json"
+  fs.unlink(path, function(err) {
+   if (err) {
+      return console.error(err);
+    }
+  else{
+    dialog.showMessageBox({ message: "The project has been deleted!", buttons: ["OK"] });
+  }
+  })
 }
 
 function createProject(filename) {
   var path = __dirname + "/Projects/" + filename + ".json"
   console.log("Going to write into existing file");
-  fs.writeFile(path,  function(err) {
-   if (err) {
+  fs.writeJSON(path,  function(err) {
+    if (err) {
       return console.error(err);
-   }
-});
+    }
+  });
+}
 
+function showProjects() {
+  console.log("Going to read directory /Projects");
+  fs.readdir(__dirname + "/Projects/",function(err, files){
+    if (err) {
+      return console.error(err);
+    }
+    var first=1;
+    files.forEach( function (file){
+      if(first==1){
+        $("<a class='nav-group-item active'>"+ file +"</a>").appendTo("#sidebar");
+        first=0;
+      }
+      else{
+        $("<a class='nav-group-item'>"+ file +"</a>").appendTo("#sidebar");
+      }
+    });
+  });
 }
 
 $(document).ready(function () {
